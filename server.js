426a9d363614404058a45b1e51912cac27470e7e
Vicente <vicenteferrer_01@hotmail.com>
2024-06-17 23:18:02 -0400

    W01 Starting

--- server.js
+++ server.js
@@ -6,9 +6,18 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+
+app.set('view engine', 'ejs')
+app.use(expressLayouts)
+app.set('layout', './layouts/layout')
 
 /* ***********************
  * Routes
@@ -26,5 +35,5 @@ const host = process.env.HOST
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`app listening on http://${host}:${port}`)
 })
